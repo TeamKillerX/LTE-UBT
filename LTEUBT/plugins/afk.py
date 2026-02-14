b1b3ce3eed314d628e25b51db73bead986489680
Randy SL <killerx@randydev.my.id>
2026-02-14 12:03:49 +0700

    Update

--- LTEUBT/plugins/afk.py
+++ LTEUBT/plugins/afk.py
@@ -4,26 +4,29 @@ from time import time
 
 @Client.on_message(
     ~filters.scheduled
-    & filters.command(["afk"], ["."])
+    & filters.command("afk", prefixes=".")
     & filters.me
     & ~filters.forwarded
 )
 async def afk(client, message):
-    start_time = time()
-    reason = message.split(" ")[1] if len(message.command) > 1 else ""
-    if reason == "":
+    start_time = str(int(time()))
+    reason = " ".join(message.command[1:]).strip()
+
+    if not reason:
         return await message.reply_text("AFK reason is missing.")
-  
+
     get_afk = await is_afk(client.me.id)
     if get_afk:
-        await message.reply_text("AFK mode is already enabled.")
-        return
+        return await message.reply_text("AFK mode is already enabled.")
+
     await set_afk_in_db(
-      user_id=client.me.id,
-      afk_time=start_time,
-      afk_reason=reason,
-      is_afk=True
+        user_id=client.me.id,
+        afk_time=start_time,
+        afk_reason=reason,
+        is_afk=True
     )
+
     return await message.reply_text(
-        f"{message.from_user.first_name} <b>is now AFK</b>\n<b>Reason:</b> <code>{reason}</code>"
+        f"{message.from_user.first_name} <b>is now AFK</b>\n"
+        f"<b>Reason:</b> <code>{reason}</code>"
     )
