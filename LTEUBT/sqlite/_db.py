05270fcd0c64889574fcc01831d0b7066a7624cd
Randy SL <killerx@randydev.my.id>
2026-02-14 10:58:08 +0700

    add: afk

--- LTEUBT/sqlite/_db.py
+++ LTEUBT/sqlite/_db.py
@@ -39,6 +39,29 @@ async def init_db():
         """)
         await db.commit()
 
+async def set_afk_in_db(user_id: int, afk_time: str, afk_reason: str, is_afk: bool):
+    async with aiosqlite.connect(DB_PATH, timeout=10) as db:
+        await db.execute("""
+            INSERT INTO afk (user_id, afk_time, afk_reason, is_afk)
+            VALUES (?, ?, ?, ?)
+            ON CONFLICT(user_id) DO UPDATE SET
+                afk_time=excluded.afk_time,
+                afk_reason=excluded.afk_reason,
+                is_afk=excluded.is_afk
+        """, (user_id, afk_time, afk_reason, int(is_afk)))
+        await db.commit()
+        return True
+
+async def is_afk(user_id: int) -> bool:
+    async with aiosqlite.connect(DB_PATH, timeout=10) as db:
+        cursor = await db.execute(
+            "SELECT is_afk FROM afk WHERE user_id=?",
+            (user_id,)
+        )
+        row = await cursor.fetchone()
+        await cursor.close()
+        return bool(row[0]) if row else False
+
 async def set_prefix_in_db(user_id: int, prefix: str):
     async with aiosqlite.connect(DB_PATH) as db:
         await db.execute('''
