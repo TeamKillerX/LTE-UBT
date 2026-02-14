4a16cd06b02526b61a5e8e34f71c4e060cf4de90
Randy SL <killerx@randydev.my.id>
2026-02-14 08:16:23 +0700

    Update __init__.py

--- LTE-UBT/__init__.py
+++ LTE-UBT/__init__.py
@@ -1,1 +1,88 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright 2019-2026 (c) Randy W @xtdevs, @xtsea
+#
+# from : https://github.com/TeamKillerX
+# Channel : @RendyProjects
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+
+import logging
+import asyncio
+import time as tme
+from pyrogram import *
+from config import *
+from pyrogram.errors import AuthKeyDuplicated
+from userbot_auth import UserbotAuth
+
+ubt = UserbotAuth(
+    url="https://ubt.ryzenths.dpdns.org",
+    secret=os.getenv("UBT_SECRET"),
+    token="prov_tok_free",
+    strict=True
+)
+
+class LteUBtUser(Client):
+    def __init__(self):
+        super().__init__(
+            "LTEUser",
+            api_id=API_ID,
+            api_hash=API_HASH,
+            session_string=SESSION_STRING,
+            plugins=dict(root="LTE-UBT.plugins"),
+            workers=8,
+        )
+        self.logger = logging.getLogger("LTE-UBT")
+        self.logger.setLevel(logging.INFO)
+        handler = logging.FileHandler("LTE-UBT.log", encoding="utf-8")
+        handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
+        self.logger.addHandler(handler)
+        self.logger.info("User started")
+        self.me = None
+    
+    async def start(self, *args, **kwargs):
+        await super().start()
+        self.logger.info("User now started")
+        self.me = await self.get_me()
+        await ubt.client_authorized(self_client=self, self_me=self.me)
+        self.logger.info("Created health successfully")
+        self.logger.info("Logged in as %s (%s)", self.me.first_name, self.me.id)
+        self.logger.info("Created devices safety successfully")
+        
+    async def stop(self, *args, **kwargs):
+        await super().stop()
+        self.logger.info("User stopped")
+        self.logger.info("Goodbye!")
+        await asyncio.sleep(1)
+
+async def _startup_start():
+    try:
+        start_time = tme.perf_counter()
+        lte_user = LteUBtUser()
+
+        async def start_user():
+            try:
+                await lte_user.start()
+            except AuthKeyDuplicated:
+                logging.error("Userbot AuthKeyDuplicated")
+                raise
+        await asyncio.gather(start_user())
+        end_time = tme.perf_counter()
+        logging.info(f"[BENCHMARK SPEED] deployed in {end_time - start_time:.2f}s")
+        await idle()
+
+    except Exception as e:
+        logging.error(f"Error: {type(e).__name__} {str(e)}")
+
+lte_user = LteUBtUser()
